1c925a05c6c67bae0cf6de906c146710b8cfd63b
satyamvishwakarma857 <satyamvishwakarma857@gmail.com>
2025-07-13 23:55:47 +0530

    added new infix evalution code

---
+++ src/St/_8infixEvaluation.java
@@ -0,0 +1,67 @@
+package St;
+
+import java.util.Stack;
+
+public class _8infixEvaluation {
+    public static void main(String[] args) {
+        System.out.println(2*5/2+3-2*8+5/5);
+        String str = "2*5/2+3-2*8+5/5";
+        Stack<Integer> val = new Stack<>();
+        Stack<Character> op = new Stack<>();
+        for(int i=0;i<str.length();i++){
+            char ch = str.charAt(i);
+            int value = (int)ch;
+            if(value>=48 && value<=57){
+                val.add(value-48);
+
+            }
+            else if(op.size()==0) op.push(ch);
+            else{
+                if(ch=='+' || ch=='-'){
+                    //work
+                    int val2 = val.pop();
+                    int val1 = val.pop();
+                    if(op.peek()=='+') val.push(val1+val2);
+                    else if(op.peek()=='-') val.push(val1-val2);
+                    else if(op.peek()=='*') val.push(val1*val2);
+                    else if(op.peek()=='/') val.push(val1/val2);
+                    op.pop();
+                    //push
+                    op.push(ch);
+                }
+                else if(ch=='*' || ch=='/'){
+                    if(op.peek()=='*' || op.peek()=='/'){
+                        //work
+                        int val2 = val.pop();
+                        int val1 = val.pop();
+                        if(op.peek()=='*') val.push(val1*val2);
+                        if(op.peek()=='/') val.push(val1/val2);
+                        op.pop();
+                        //push
+                        op.push(ch);
+
+                    }
+                    else op.push(ch);
+
+                }
+            }
+
+        }
+        while(val.size()>1){
+            int val2 = val.pop();
+            int val1 = val.pop();
+            if(op.peek()=='+') val.push(val1+val2);
+            if(op.peek()=='-') val.push(val1-val2);
+            if(op.peek()=='*') val.push(val1*val2);
+            if(op.peek()=='/') val.push(val1/val2);
+            op.pop();
+            //push
+
+
+        }
+        System.out.println(val.peek());
+
+
+
+    }
+}
